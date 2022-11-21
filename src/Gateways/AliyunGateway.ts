5c8d553e90b05c74e89a9e4c70bc69b5985d398f
Hpyer <me@hpyer.cn>
2022-11-21 17:16:35 +0800

    fix: 优化阿里云短信服务网关

--- src/Gateways/AliyunGateway.ts
+++ src/Gateways/AliyunGateway.ts
@@ -1,13 +1,16 @@
 'use strict';
 
 import GatewayErrorException from "../Core/Exceptions/GatewayErrorException";
-import GatewayInterface from "../Core/Gateway";
+import Gateway from "../Core/Gateway";
 import Message from "../Core/Message";
 import PhoneNumber from "../Core/PhoneNumber";
 import { createHmac, randomString, timestampUTC } from "../Core/Support/Utils";
-import { GatewayConfig } from "../Types/global";
 
-export default class AliyunGateway extends GatewayInterface {
+/**
+ * 阿里云短信服务
+ * @see https://help.aliyun.com/document_detail/419273.html
+ */
+export default class AliyunGateway extends Gateway {
 
   public static ENDPOINT_URL: string = 'http://dysmsapi.aliyuncs.com';
   public static ENDPOINT_METHOD: string = 'SendSms';
@@ -16,14 +19,14 @@ export default class AliyunGateway extends GatewayInterface {
   public static ENDPOINT_SIGNATURE_METHOD: string = 'HMAC-SHA1';
   public static ENDPOINT_SIGNATURE_VERSION: string = '1.0';
 
-  async send(to: PhoneNumber, message: Message, config: GatewayConfig) {
+  async send(to: PhoneNumber, message: Message) {
     let data = await message.getData(this);
-    let signName = data['sign_name'] ?? config['sign_name'] ?? '';
+    let signName = data['sign_name'] ?? this.config['sign_name'] ?? '';
     delete data['sign_name'];
 
     let query = {
-      RegionId: config['region'] || 'cn-hangzhou',
-      AccessKeyId: config['access_key_id'] || '',
+      RegionId: this.config['region'] || 'cn-hangzhou',
+      AccessKeyId: this.config['access_key_id'] || '',
       Format: AliyunGateway.ENDPOINT_FORMAT,
       SignatureMethod: AliyunGateway.ENDPOINT_SIGNATURE_METHOD,
       SignatureVersion: AliyunGateway.ENDPOINT_SIGNATURE_VERSION,
@@ -37,7 +40,7 @@ export default class AliyunGateway extends GatewayInterface {
       TemplateParam: JSON.stringify(data),
     };
 
-    query['Signature'] = this.generateSign(query, config['access_key_secret']);
+    query['Signature'] = this.generateSign(query, this.config['access_key_secret']);
 
     let result = await this.get(AliyunGateway.ENDPOINT_URL, query);
 
