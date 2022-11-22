7db7a424686cc49617adb35377046741113a7a9e
Hpyer <me@hpyer.cn>
2022-11-22 20:47:01 +0800

    fix: 导出异常类

--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,18 @@ import Gateway from './Core/Gateway';
 import Message from './Core/Message';
 import Messenger from './Core/Messenger';
 import PhoneNumber from './Core/PhoneNumber';
+import Exception from './Core/Exceptions/Exception';
+import GatewayErrorException from './Core/Exceptions/GatewayErrorException';
+import NoGatewayAvailableException from './Core/Exceptions/NoGatewayAvailableException';
 
-export = {
+export {
   EasySms,
   Gateway,
   Message,
   Messenger,
   PhoneNumber,
+
+  Exception,
+  GatewayErrorException,
+  NoGatewayAvailableException,
 };
