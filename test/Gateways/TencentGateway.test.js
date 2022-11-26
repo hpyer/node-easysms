64f73df29635c77cd3cf8af9379d7673fee8a70e
Hpyer <me@hpyer.cn>
2022-11-26 21:42:23 +0800

    test: 增加腾讯云签名测试用例

--- test/Gateways/TencentGateway.test.js
+++ test/Gateways/TencentGateway.test.js
@@ -6,6 +6,30 @@ class TestUnit extends BaseTestUnit {
 
   test() {
 
+    it('Should generate signature correctly.', async () => {
+
+      let gateway = new TencentGateway({
+        sdk_app_id: 'mock-appid',
+        secret_id: 'AKIDz8krbsJ5yKBZQpn74WFkmLPx3*******',
+        secret_key: 'Gu5t9xGARNpq86cd98joQYCN3*******',
+        region: 'ap-guangzhou',
+        sign_name: 'mock-sign-name',
+      });
+
+      let time = '1551113065';
+
+      let sign = gateway.generateSign({
+        PhoneNumberSet: ['13812345678'],
+        SmsSdkAppId: 'mock-sdk_app_id',
+        SignName: 'xx平台',
+        TemplateId: 'mock-template_id',
+        TemplateParamSet: ['foo', 'bar'],
+      }, time);
+
+      this.assert.strictEqual(sign, 'TC3-HMAC-SHA256 Credential=AKIDz8krbsJ5yKBZQpn74WFkmLPx3*******/2019-02-25/sms/tc3_request, SignedHeaders=content-type;host, Signature=ceaef92817668dcd32d328fa30a0b4612a3b10cdb1cff1f625baaf77f027915d');
+
+    });
+
     it('Should return send correctly.', async () => {
       let to = new PhoneNumber('13812345678');
       let message = new Message({
